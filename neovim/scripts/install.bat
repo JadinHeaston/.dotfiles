37bebf8c3203d6466002e234d2816f1e81a26b64
Jadin Heaston <jadin.heaston@como.gov>
2025-03-27 12:35:30 -0500

    Adding Alacritty install and comments.
    Signed-off-by: Jadin Heaston <jadin.heaston@como.gov>

--- neovim/scripts/install.bat
+++ neovim/scripts/install.bat
@@ -1,5 +1,10 @@
+REM Installing compatible terminal.
+winget install -e --id Alacritty.Alacritty
+
+REM NeoVIM
 winget install --id=Neovim.Neovim  -e
 
+REM LazyGit, so it works with the integration.
 winget install -e --id=JesseDuffield.lazygit
 
 REM winget install SSHFS-Win.SSHFS-Win
