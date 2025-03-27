7973b566c45d4c74eb4e4639f5ea7a9ab5105a1e
Jadin Heaston <jadin.heaston@como.gov>
2025-03-27 13:03:14 -0500

    Adding -i flag to LLVM install to add LLVM to PATH.
    Runs the LLVM GUI for selecting PATH install.
    
    Signed-off-by: Jadin Heaston <jadin.heaston@como.gov>

--- neovim/scripts/install.bat
+++ neovim/scripts/install.bat
@@ -4,8 +4,8 @@ winget install -e --id Alacritty.Alacritty
 REM NeoVIM
 winget install --id=Neovim.Neovim  -e
 
-REM Installing tools for plugin installs.
-winget install -e --id LLVM.LLVM
+REM Installing tools for plugin installs. - It's important to register to PATH during the GUI install.
+winget install -e -i --id LLVM.LLVM
 
 REM LazyGit, so it works with the integration.
 winget install -e --id=JesseDuffield.lazygit
