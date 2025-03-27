efdd0b73d0743963d1dfe3a9789fb9ec5ae09ade
Jadin Heaston <jadin.heaston@como.gov>
2025-03-27 12:38:38 -0500

    Adding LLVM to Windows install script for plugin installation.
    Signed-off-by: Jadin Heaston <jadin.heaston@como.gov>

--- neovim/scripts/install.bat
+++ neovim/scripts/install.bat
@@ -4,6 +4,9 @@ winget install -e --id Alacritty.Alacritty
 REM NeoVIM
 winget install --id=Neovim.Neovim  -e
 
+REM Installing tools for plugin installs.
+winget install -e --id LLVM.LLVM
+
 REM LazyGit, so it works with the integration.
 winget install -e --id=JesseDuffield.lazygit
 
