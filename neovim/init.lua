de3dd7fab5d4223770535cc1b405c01fc043e388
Jadin Heaston <86203688+JadinHeaston@users.noreply.github.com>
2025-04-12 14:38:22 -0500

    Moving from NVIM Tree to Oil for file tree functionality.

--- neovim/init.lua
+++ neovim/init.lua
@@ -846,15 +846,60 @@ require("lazy").setup({
 		--    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
 		--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
 	},
-	{ -- File explorer.
-		"nvim-tree/nvim-tree.lua",
-		version = "*",
-		lazy = false,
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
+	{ -- File explorer/editor
+		"stevearc/oil.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
-			require("nvim-tree").setup {}
+			CustomOilBar = function()
+				local path = vim.fn.expand("%")
+				path = path:gsub("oil://", "")
+
+				return "  " .. vim.fn.fnamemodify(path, ":.")
+			end
+
+			local detail = false -- Sets default detail state.
+			require("oil").setup({
+				columns = { "icon" },
+				keymaps = {
+					["<C-h>"] = false,
+					["<C-l>"] = false,
+					["<C-k>"] = false,
+					["<C-j>"] = false,
+					["<M-h>"] = "actions.select_split",
+					["gd"] = {
+						desc = "Toggle file detail view",
+						callback = function()
+							detail = not detail
+							if detail then
+								require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+							else
+								require("oil").set_columns({ "icon" })
+							end
+						end,
+					},
+				},
+				win_options = {
+					winbar = "%{v:lua.CustomOilBar()}",
+				},
+				view_options = {
+					show_hidden = true,
+					is_always_hidden = function(name, _)
+						local folder_skip = { "dev-tools.locks", "dune.lock", "_build" }
+						return vim.tbl_contains(folder_skip, name)
+					end,
+				},
+			})
+
+			-- Open parent directory in current window
+			vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+			-- Open parent directory in floating window
+			vim.keymap.set(
+				"n",
+				"<space>-",
+				require("oil").toggle_float,
+				{ desc = "Open parent directory (Oil - Floating)" }
+			)
 		end,
 	},
 	{ -- Adds context lines (Sticky Scroll) at the top when browsing nested content.
