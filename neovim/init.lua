9b9e8dd8371ec1a7282eda3ccf8787b08ae80d8c
Jadin Heaston <86203688+JadinHeaston@users.noreply.github.com>
2025-04-12 12:16:03 -0500

    Disabling Git Blame by default and changing how it is displayed to prevent text cutoff.

--- neovim/init.lua
+++ neovim/init.lua
@@ -896,8 +896,8 @@ require("lazy").setup({
 		-- load the plugin at startup
 		event = "VeryLazy",
 		opts = {
-			enabled = true,   -- if you want to enable the plugin
-			virtual_text_column = 120, -- virtual text start column, check Start virtual text at column section for more options
+			enabled = false, -- if you want to enable the plugin
+			virtual_text_column = 80, -- virtual text start column, check Start virtual text at column section for more options
 
 			message_template = "<summary> • <date> • <author> • <<sha>>",
 			message_when_not_committed = "Not Committed",
