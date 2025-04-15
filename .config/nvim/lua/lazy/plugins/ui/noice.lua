6f783202110006c0ed2c0a07defcbbc8da50f526
Jadin Heaston <86203688+JadinHeaston@users.noreply.github.com>
2025-04-14 20:47:39 -0500

    Fixing notifications...
    Signed-off-by: Jadin Heaston <86203688+JadinHeaston@users.noreply.github.com>

--- .config/nvim/lua/lazy/plugins/ui/noice.lua
+++ .config/nvim/lua/lazy/plugins/ui/noice.lua
@@ -6,7 +6,7 @@ return {
 			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 			"MunifTanjim/nui.nvim",
 			-- OPTIONAL: `nvim-notify` is only needed, if you want to use the notification view. `mini` is the fallback
-			-- "rcarriga/nvim-notify",
+			"rcarriga/nvim-notify",
 		},
 		opts = {
 			lsp = {
