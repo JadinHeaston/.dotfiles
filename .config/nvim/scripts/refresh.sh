09f06431f1fe06d6aff619de03c47b6151c27912
Jadin Heaston <86203688+JadinHeaston@users.noreply.github.com>
2025-04-17 21:31:13 -0500

    Fixing refresh.sh quoting.
    Signed-off-by: Jadin Heaston <86203688+JadinHeaston@users.noreply.github.com>

--- .config/nvim/scripts/refresh.sh
+++ .config/nvim/scripts/refresh.sh
@@ -1,5 +1,5 @@
 script_dir=$(dirname "$0")
 
-mkdir -p "~/.config/nvim/"
-rm -rf "~/.config/nvim/*"
-cp -rf "$script_dir/../" "~/.config/nvim/"
+mkdir -p ~/.config/nvim/
+rm -rf ~/.config/nvim/*
+cp -rf $script_dir/../ ~/.config/nvim/
