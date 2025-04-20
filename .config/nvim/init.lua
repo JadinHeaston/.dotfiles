c238933958f3cfa342e2e79cec5afe98a0bf68b6
Jadin Heaston <86203688+JadinHeaston@users.noreply.github.com>
2025-04-19 23:12:23 -0500

    Formatting

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -35,16 +35,16 @@ if vim.fn.has("win32") == 1 then
 end
 
 -- [[ Setting options ]]
-require 'options'
+require("options")
 
 -- [[ Keymaps ]]
-require 'keymaps'
+require("keymaps")
 
 -- [[ Install `lazy.nvim` plugin manager ]]
-require 'lazy-bootstrap'
+require("lazy-bootstrap")
 
 -- [[ Configure and install plugins ]]
-require 'lazy-plugins'
+require("lazy-plugins")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
