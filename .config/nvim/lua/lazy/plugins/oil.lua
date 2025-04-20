d26ba850401f1141971c78b4354aa5ec3dcf0a54
Jadin Heaston <86203688+JadinHeaston@users.noreply.github.com>
2025-04-19 23:49:15 -0500

    Adding preview by default and formatting.

--- .config/nvim/lua/lazy/plugins/oil.lua
+++ .config/nvim/lua/lazy/plugins/oil.lua
@@ -1,58 +1,56 @@
 return {
-	{
-		"stevearc/oil.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			CustomOilBar = function()
-				local path = vim.fn.expand("%")
-				path = path:gsub("oil://", "")
+	"stevearc/oil.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		CustomOilBar = function()
+			local path = vim.fn.expand("%")
+			path = path:gsub("oil://", "")
 
-				return "  " .. vim.fn.fnamemodify(path, ":.")
-			end
+			return "  " .. vim.fn.fnamemodify(path, ":.")
+		end
 
-			local detail = false -- Sets default detail state.
-			require("oil").setup({
-				columns = { "icon" },
-				keymaps = {
-					["<C-h>"] = false,
-					["<C-l>"] = false,
-					["<C-k>"] = false,
-					["<C-j>"] = false,
-					["<M-h>"] = "actions.select_split",
-					["gd"] = {
-						desc = "Toggle file detail view",
-						callback = function()
-							detail = not detail
-							if detail then
-								require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
-							else
-								require("oil").set_columns({ "icon" })
-							end
-						end,
-					},
-				},
-				win_options = {
-					winbar = "%{v:lua.CustomOilBar()}",
-				},
-				view_options = {
-					show_hidden = true,
-					is_always_hidden = function(name, _)
-						local folder_skip = { "dev-tools.locks", "dune.lock", "_build" }
-						return vim.tbl_contains(folder_skip, name)
+		local detail = true -- Sets default detail state.
+		require("oil").setup({
+			columns = { "icon", "permissions", "size", "mtime" },
+			keymaps = {
+				["<C-h>"] = false,
+				["<C-l>"] = false,
+				["<C-k>"] = false,
+				["<C-j>"] = false,
+				["<M-h>"] = "actions.select_split",
+				["gd"] = {
+					desc = "Toggle file detail view",
+					callback = function()
+						detail = not detail
+						if detail then
+							require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+						else
+							require("oil").set_columns({ "icon" })
+						end
 					end,
 				},
-			})
+			},
+			win_options = {
+				winbar = "%{v:lua.CustomOilBar()}",
+			},
+			view_options = {
+				show_hidden = true,
+				is_always_hidden = function(name, _)
+					local folder_skip = { "dev-tools.locks", "dune.lock", "_build" }
+					return vim.tbl_contains(folder_skip, name)
+				end,
+			},
+		})
 
-			-- Open parent directory in current window
-			vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+		-- Open parent directory in current window
+		vim.keymap.set("n", "-", "<CMD>Oil --preview<CR>", { desc = "Open parent directory" })
 
-			-- Open parent directory in floating window
-			vim.keymap.set(
-				"n",
-				"<space>-",
-				require("oil").toggle_float,
-				{ desc = "Open parent directory (Oil - Floating)" }
-			)
-		end,
-	},
+		-- Open parent directory in floating window
+		vim.keymap.set(
+			"n",
+			"<space>-",
+			require("oil").toggle_float,
+			{ desc = "Open parent directory (Oil - Floating)" }
+		)
+	end,
 }
