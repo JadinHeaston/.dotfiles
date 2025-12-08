26856aada32e7544334ab7c326a09b74ee631bbd
Jadin Heaston <jadin.heaston@como.gov>
2025-12-08 11:40:59 -0600

    Setting PowerShell as default shell for Windows.

--- .config/nvim/lua/os/win.lua
+++ .config/nvim/lua/os/win.lua
@@ -1,2 +1,8 @@
 -- Disabling font due to graphical errors.
 vim.g.have_nerd_font = false
+
+if vim.fn.executable("pwsh") == 1 then
+	vim.opt.shell = "pwsh" --"pwsh" for 7.x if installed
+else
+	vim.opt.shell = "powershell" --"powershell" for 5.x
+end
